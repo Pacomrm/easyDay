773847e693c34f8ed58d6aa335e13c30845d4ea6
Pacomrm <rodriguez.pacorro@gmail.com>
2015-02-21 14:44:16 -0600

    fullead

--- app/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	'port': process.env.PORT || 8080,
-	//'database': 'mongodb://michael:michael123@novus.modulusmongo.net:27017/avo7qadA',
-	'database': 'mongodb://localhost/test',
-    'secret': 'claveclaveclave'
-};
